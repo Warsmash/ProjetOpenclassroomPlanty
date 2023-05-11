d16752ac7b08c5aaf9f89cec0bac72c666e5c0eb
Bruno L <brunolelay2000@gmail.com>
2023-05-11 19:34:11 +0200

    test push

---
+++ wp-content/themes/oceanwp-child-theme-master/footer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package OceanWP WordPress theme
+ */
+
+?>
+
+<footer id="site-footer" class="header-footer-group">
+              <div>
+				<a href="#">Mentions légales</a>
+			  </div>
+        </footer><!-- #site-footer -->
+
+      <?php wp_footer(); ?>
+
+   </body>
+</html>
+
